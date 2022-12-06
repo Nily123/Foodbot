7dafb185cb1c9a0aaded6c192424a5eddea67420
Nily123 <119929004+Nily123@users.noreply.github.com>
2022-12-07 00:42:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,22 @@ def translate_text(text,de):
     result = translator.translate(text, dest=de).text
     return result
 
+def ask(q):
+    import openai
+    openai.api_key = "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"                   
+    response = openai.Completion.create(
+    model="foodbot",
+    prompt=q,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=["END"]
+    )                     
+    story = response['choices'][0]['text'] 
+    return story
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
